71f26be953f207474c076c5c044e62cf78dbe886
Nirmal, Subramanian <nirmal.subramanian@staples.com>
2023-10-05 15:18:48 -0400

    Coding Login feature

--- my-task-manager/pages/index.tsx
+++ my-task-manager/pages/index.tsx
@@ -1,5 +1,5 @@
 // import button and Typograpgy from mateiral ui
-import { Button, Typography, TextField, Card, CardContent } from "@mui/material";
+import { Button, Card, CardContent, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 
 
@@ -15,15 +15,17 @@ const home = () => {
     }
 
     return (
-        <Card sx={{ minWidth: 400, width: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} variant="outlined">
-            <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-                <Typography align="center" sx={{ fontSize: 24 }} color="secondary" gutterBottom>
-                    Welcome to Task Manager
-                </Typography>
-                <Button variant="contained" onClick={handleLoginClick}>Login</Button>
-                <Button variant="outlined" onClick={handleRegisterClick}>Register</Button>
-            </CardContent>
-        </Card>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Card sx={{ minWidth: 400, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} variant="outlined">
+                <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+                    <Typography align="center" sx={{ fontSize: 24 }} color="secondary" gutterBottom>
+                        Welcome to Task Manager
+                    </Typography>
+                    <Button variant="contained" onClick={handleLoginClick}>Login</Button>
+                    <Button variant="outlined" onClick={handleRegisterClick}>Register</Button>
+                </CardContent>
+            </Card>
+        </div>
     );
 };
 
--- my-task-manager/pages/login.tsx
+++ my-task-manager/pages/login.tsx
@@ -1,5 +1,92 @@
-export default function Login() {
-    return (
-        <div>This is Login page</div>
-    );
+import { useState } from 'react';
+import { Button, Card, CardContent, TextField, Typography } from '@mui/material';
+import { useRouter } from 'next/router';
+
+interface LoginData {
+    email: string;
+    password: string;
 }
+
+const Login = () => {
+    const router = useRouter();
+    const [loginData, setLoginData] = useState<LoginData>({ email: '', password: '' });
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setLoginData(prevState => ({ ...prevState, [name]: value }));
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(loginData);
+        // make API call to login
+        try {
+            const response = await fetch('http://localhost:3001/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(loginData),
+            });
+
+            console.log(response);
+            if(response.ok) {
+                router.push('/home');
+            } else {
+                console.log('Login failed');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        // if success, redirect to home page
+
+    };
+
+    const handleGoBackClick = () => {
+        router.back();
+    };
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Card sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} variant="outlined">
+
+                <form onSubmit={handleSubmit} >
+                    <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: 1 }}>
+
+                        <Typography variant="h4" align="center" gutterBottom>
+                            Login
+                        </Typography>
+                        <TextField
+                            label="Email"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            name="email"
+                            value={loginData.email}
+                            onChange={handleInputChange}
+                        />
+                        <TextField
+                            label="Password"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            name="password"
+                            type="password"
+                            value={loginData.password}
+                            onChange={handleInputChange}
+                        />
+                        <Button variant="contained" color="primary" type="submit">
+                            Login
+                        </Button>
+                        <Button variant="outlined" onClick={handleGoBackClick}>
+                            Go Back
+                        </Button>
+                    </CardContent>
+                </form>
+
+            </Card>
+        </div>
+    );
+};
+
+export default Login;
